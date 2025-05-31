517c9b95999d57a8f2cc599790b295f9aa7e39b6
gpego1 <gpego786@gmail.com>
2025-05-31 12:32:21 -0300

    Actuator implementado

--- src/main/java/br/com/project/music/config/SecurityConfig.java
+++ src/main/java/br/com/project/music/config/SecurityConfig.java
@@ -118,7 +118,8 @@ public class SecurityConfig {
                 "/swagger-resources/**",
                 "/swagger-ui.html",
                 "/swagger-ui/**",
-                "/webjars/**"
+                "/webjars/**",
+                "/actuator/**"
         );
     };
 }
